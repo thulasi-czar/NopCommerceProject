89f8aa5f6ea9eebc4f4d1f7a2266f7d3a7bcf09b
thulasi-czar <ayaanashok143.m@gmail.com>
2023-11-09 18:28:32 +0530

    second commit

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-pytest .\testcases\ --html=".\\Reports\\Html_Reports\\ReportOne.html" --alluredir=".\\Reports\\Allure_Reports\\ReportOne" --browser=chrome
+pytest .\testCases --html=".\\Reports\\Html_Reports\\ReportOne.html" --alluredir=".\\Reports\\Allure_Reports\\ReportOne" --browser=chrome
